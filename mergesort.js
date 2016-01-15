b523629d07bf8b1457da2264e0d1a3328755ee08
Assaf Packin <apackin@gmail.com>
2016-01-15 12:04:30 -0500

    Update mergesort.js

--- mergesort.js
+++ mergesort.js
@@ -1,21 +1,42 @@
-var merge = function(array, array2) {
+// var merge = function(array, array2) {
+//    var arrOut = [];
+//    if(array[0]>array2[0]){arrOut.push(array2.shift());}
+//    else{arrOut.push(array.shift());}
+//    if(!array.length || !array2.length) {
+//        return arrOut.concat(array).concat(array2);
+//    }
+//    //use for loop instead
+
+//    arrOut = arrOut.concat(merge(array, array2));
+//    return arrOut;
+// };
+
+
+var merge = function(left, right) {
    var arrOut = [];
-   if(array[0]>array2[0]){arrOut.push(array2.shift());}
-   else{arrOut.push(array.shift());}
-   if(!array.length || !array2.length) {
-       return arrOut.concat(array).concat(array2);
+   var leftIdx = 0;
+   var rightIdx = 0;
+
+   while(left.length !== leftIdx && right.length !== rightIdx) {
+      if(left[leftIdx]<right[rightIdx]) {
+         arrOut.push(left[leftIdx]);
+         leftIdx++;
+      } else {
+         arrOut.push(right[rightIdx]);
+         rightIdx++;
+      }
    }
-   //use for loop instead
 
-   arrOut = arrOut.concat(merge(array, array2));
-   return arrOut;
-
+   return arrOut.concat(left.splice(leftIdx)).concat(right.splice(rightIdx));
 };
+
 
 var split = function(wholeArray){
 	if(wholeArray.length===1) {return wholeArray;}
 
-	var sliceSpot = Math.ceil(wholeArray.length/2);
+	var sliceSpot = wholeArray.length/2;
+
+   //don't need Math.ceil or Math.floor b/c the slice command already Math.floors inputs.
 
 	return [wholeArray.slice(0,sliceSpot) , wholeArray.slice(sliceSpot)];
 
@@ -23,7 +44,6 @@ var split = function(wholeArray){
 
 var mergeSort = function(arrayToSort){
 	if (arrayToSort.length===1) {return arrayToSort;}
-
    return merge(mergeSort(split(arrayToSort)[0]),mergeSort(split(arrayToSort)[1]));
 
 	// var splitArray = split(arrayToSort);
