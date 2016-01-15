143850181abf5e68fe70b665bcb7c87c9a7ec381
Assaf Packin <apackin@gmail.com>
2016-01-15 12:04:55 -0500

    Update mergesort.spec.js
    Added Uneven test

--- mergesort.spec.js
+++ mergesort.spec.js
@@ -2,6 +2,10 @@ describe('Merge Sort', function(){
     it('is able to merge two sorted arrays', function(){
        expect(merge([4, 23], [8, 15])).toEqual([4, 8, 15, 23]);
     });
+
+    it('is able to merge two sorted uneven length arrays', function(){
+       expect(merge([4, 23, 24, 25, 30, 40], [8, 15])).toEqual([4, 8, 15, 23, 24, 25, 30, 40]);
+    });
 });
 
 describe('Split Array function', function() {
@@ -13,5 +17,9 @@ describe('Split Array function', function() {
 describe('mergeSort Array', function() {
   it('takes in an array and returns a sorted array', function() {
     expect(mergeSort([23, 4, 15, 8])).toEqual([4, 8, 15, 23]);
+  });
+
+   it('takes in an odd array and returns a sorted array', function() {
+    expect(mergeSort([23, 4, 15, 8, 75])).toEqual([4, 8, 15, 23, 75]);
   });
 });
